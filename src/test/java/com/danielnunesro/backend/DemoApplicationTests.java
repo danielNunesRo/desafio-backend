f368a3dab39a170d5e7f24bbe7bf87b29767e126
Daniel Nunes <danielnunespk@gmail.com>
2024-06-12 09:58:24 -0300

    Initialize project setup

---
+++ src/test/java/com/danielnunesro/backend/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielnunesro.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
